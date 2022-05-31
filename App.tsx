ae26c23b8a32cebd5c31d950553d90b7f71501f3
Luiz Henrique <henryque.souz4@gmail.com>
2022-05-31 19:27:40 -0400

    feat: add lint config

--- App.tsx
+++ App.tsx
@@ -1,15 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { Center,  NativeBaseProvider } from 'native-base';
+import { Center, NativeBaseProvider } from 'native-base';
 
 export default function App() {
   return (
     <NativeBaseProvider>
-
       <Center flex={1}>
-      <StatusBar style="auto" />
-      My Pass
+        <StatusBar style="auto" />
+        My Pass
       </Center>
     </NativeBaseProvider>
   );
 }
-
